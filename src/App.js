f2673bd9b464a64dbf110ad9d18c62f3ab271c42
Reverie-PC <reverie-pc@Sidharths-MacBook-Pro.local>
2019-08-07 23:29:35 +0530

    n

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={image} className="App-logo" alt="logo" />
-        <p>Opps.!! You are in right place, but we are under construction.</p>
-        <p>Keep watching this space ...</p>
+        <p style={{color:"#596275"}}>Oops.!! You are in right place, but we are under construction.</p>
+        <p style={{fontWeight:"500", color:"rgb(104, 131, 115)"}}> <i className="fa fa-graduation-cap"></i>&nbsp;&nbsp;Keep Learning...</p>
+        
       </header>
     </div>
   );
